622c87805b66207991a81371b39cf7c8809e4c27
Mathieu Tuli <tuli.mathieu@gmail.com>
2020-06-25 11:30:15 -0400

    Refactor test_main to its own file

--- src/adas/train.py
+++ src/adas/train.py
@@ -39,6 +39,7 @@ from .early_stop import EarlyStop
 from .profiler import Profiler
 from .metrics import Metrics
 from .models import get_net
+from .test import test_main
 from .data import get_data
 from .AdaS import AdaS
 
@@ -310,52 +311,6 @@ def main(args: APNamespace):
                 print("AdaS: Early stop activated.")
                 break
     return
-
-
-def test_main(test_loader, epoch: int, device) -> Tuple[float, float]:
-    # global best_acc, performance_statistics, net, criterion, checkpoint_path
-    GLOBALS.NET.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(test_loader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = GLOBALS.NET(inputs)
-            loss = GLOBALS.CRITERION(outputs, targets)
-
-            test_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-
-            # progress_bar(
-            #     batch_idx, len(test_loader),
-            #     'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            #     % (test_loss / (batch_idx + 1), 100. * correct / total,
-            #        correct, total))
-
-    # Save checkpoint.
-    acc = 100. * correct / total
-    if acc > GLOBALS.BEST_ACC:
-        # print('Adas: Saving checkpoint...')
-        state = {
-            'net': GLOBALS.NET.state_dict(),
-            'acc': acc,
-            'epoch': epoch + 1,
-        }
-        if GLOBALS.ADAS is not None:
-            state['historical_io_metrics'] = GLOBALS.METRICS.historical_metrics
-        torch.save(state, str(GLOBALS.CHECKPOINT_PATH / 'ckpt.pth'))
-        # if checkpoint_path.is_dir():
-        #     torch.save(state, str(checkpoint_path / 'ckpt.pth'))
-        # else:
-        #     torch.save(state, str(checkpoint_path))
-        GLOBALS.BEST_ACC = acc
-    GLOBALS.PERFORMANCE_STATISTICS[f'test_acc_epoch_{epoch}'] = acc / 100
-    GLOBALS.PERFORMANCE_STATISTICS[f'test_loss_epoch_{epoch}'] = \
-        test_loss / (batch_idx + 1)
-    return test_loss / (batch_idx + 1), acc
 
 
 @Profiler
