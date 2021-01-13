80e38ee4e5fe74f17ae5ce1a03f0232ac3336a34
Mathieu Tuli <tuli.mathieu@gmail.com>
2021-01-13 17:18:22 -0500

    Update 2d - working code. issues with no progress

--- src/autohyper/autoHyper.py
+++ src/autohyper/autoHyper.py
@@ -1,3 +1,4 @@
+
 """
 MIT License
 
@@ -23,6 +24,7 @@ SOFTWARE.
 """
 from typing import Union, List
 from datetime import datetime
+import itertools
 import sys
 
 # import logging
@@ -52,10 +54,9 @@ def auto_lr(training_agent,
             num_split: int = 20,
             min_delta: float = 5e-3,
             scale_delta: float = 3e-5,
-            epochs: Union[range, List[int]] = range(0, 5),
+            epochs: Union[range, List[int]] = range(0, 1),
             power: float = 0.8):
     establish_start = True
-    output_history = list()
     cur_rank = -1
     auto_lr_path = training_agent.output_path / 'auto-lr'
     auto_lr_path.mkdir(exist_ok=True)
@@ -65,10 +66,17 @@ def auto_lr(training_agent,
         delta[param] = .1
         if param == 'init_lr':
             training_agent.config['init_lr'] = \
-                hyper_parameters.config[param]['current']
+                hyper_parameters.config[param].current
         elif param == 'weight_decay':
             training_agent.config['optimizer_kwargs']['weight_decay'] = \
-                hyper_parameters.config[param]['current']
+                hyper_parameters.config[param].current
+
+    num_hp = len(hyper_parameters.config.keys())
+    trust_buffer = np.full([3] * num_hp, -1., dtype=float)
+    rank_history = list()
+    scale_powers = [-1, 0, 1]  # defines the trust region
+    trust_region = list(itertools.product(
+        *[scale_powers for i in range(num_hp)]))
 
     def reset():
         training_agent.reset(training_agent.config)
@@ -89,74 +97,116 @@ def auto_lr(training_agent,
             ".csv".replace(' ', '-')
         training_agent.output_filename = str(
             training_agent.output_filename / string_name)
-    rank_buffer = np.full_like(3 ** len(hyper_parameters.config.keys()), -1)
-    rank_memory = list()
+    # for 2 hyper-parameters and 3 hyper-parameters,
+    # the buffer is a 3x3 matrix, a 3x3x3 cube, a ...
+
     while True:
-        if establish_start and not(np.isclose(cur_rank, 1.0, atol=.1)):
+        print('--')
+        print("Current settings:")
+        print(f"trust_buffer: {trust_buffer}")
+        print(f"rank_history: {rank_history}")
+        print(f"params: {hyper_parameters.config}")
+        print('--')
+
+        if establish_start:
             reset()
             training_agent.run_epochs(trial=0, epochs=epochs)
             cur_rank = compute_rank(training_agent.metrics)
-            print("FIRST RUN LESS THAN 90%")
-            for param in hyper_parameters.config.keys():
-                hyper_parameters.config[param]['current'] /= 10
-                if param == 'init_lr':
-                    training_agent.config['init_lr'] = \
-                        hyper_parameters.config[param]['current']
-                elif param == 'weight_decay':
-                    training_agent.config['optimizer_kwargs']['weight_decay'
-                                                              ] = \
-                        hyper_parameters.config[param]['current']
-            continue
+            if np.less(cur_rank, .9):
+                print("FIRST RUN LESS THAN 90%")
+                print(f"Rank was {cur_rank}")
+                for param in hyper_parameters.config.keys():
+                    hyper_parameters.config[param].current /= \
+                        hyper_parameters.config[param].scale ** 2
+                    if param == 'init_lr':
+                        training_agent.config['init_lr'] = \
+                            hyper_parameters.config[param].current
+                    elif param == 'weight_decay':
+                        training_agent.config['optimizer_kwargs']['weight_decay'
+                                                                  ] = \
+                            hyper_parameters.config[param].current
+                continue
         establish_start = False
+        for scale_power in trust_region:
+            print('--')
+            print("Doing TR")
+            print(f"params: {hyper_parameters.config}")
+            print(f"trust_region: {trust_buffer}")
+            print(f"params: {training_agent.config}")
+            index = tuple(np.array(scale_power) + 1)
+            if np.less(trust_buffer[index], 0.):
+                for i, param in enumerate(hyper_parameters.config.keys()):
+                    current = hyper_parameters.config[param].current * \
+                        (hyper_parameters.config[param].scale **
+                         scale_power[i])
+                    if param == 'init_lr':
+                        training_agent.config['init_lr'] = current
+                    elif param == 'weight_decay':
+                        training_agent.config['optimizer_kwargs'][
+                            'weight_decay'] = current
+                reset()
+                training_agent.run_epochs(trial=0, epochs=epochs)
+                cur_rank = compute_rank(training_agent.metrics)
+                print(f"Rank was {cur_rank}")
+                print('--')
+                trust_buffer[index] = cur_rank
+            # if np.less(trust_buffer, 0.).any():  # not done populating buffer
+            #     continue
+        # TODO only works for 2D cse
+        if np.equal(trust_buffer, 1.).all():
+            min_index = (2, 2)
+        else:
+            min_index = np.unravel_index(
+                np.argmin(trust_buffer), trust_buffer.shape)
+        print(f'min_index: {min_index}')
+        rank_history.append(np.min(trust_buffer))
+        for axis, i in enumerate(min_index):
+            mid = int(trust_buffer.shape[axis] / 2)
+            trust_buffer = np.roll(trust_buffer, (i - mid) * -1, axis=axis)
+        # TODO THIS ONLY WORKS FOR 2D MATRIX
+        if min_index[0] == 0 or min_index[0] == trust_buffer.shape[0] - 1:
+            trust_buffer[min_index[0], :] = -1.
+        if min_index[1] == 0 or min_index[1] == trust_buffer.shape[1] - 1:
+            trust_buffer[:, min_index[1]] = -1.
+        print(f'trust_buffer: {trust_buffer}')
+        scale_power = list(np.array(min_index) - 1)
         for i, param in enumerate(hyper_parameters.config.keys()):
-            for scale in [0, 1, -1]:
-                # if hyper_parameters.config[param]['stop']:
-                #     continue
-                if np.all(rank_buffer < 0):
-                    reset()
-                    training_agent.run_epochs(trial=0, epochs=epochs)
-                    cur_rank = compute_rank(training_agent.metrics)
-                    rank_buffer
-                if np.isclose(cur_rank, 0.0):
-                    print("RANK ZERO BACK UP")
-                    hyper_parameters.config[param]['current'] *= \
-                        hyper_parameters.config[param]['scale'] ** 2
-                    continue
-                hyper_parameters.config[param]['buffer'].append(cur_rank)
-                zeta = np.cumprod(
-                    hyper_parameters.config[param]['buffer']) ** power
-                if np.less(zeta[-1], min_delta):
-                    print("LESS THAN DELTA")
-                    if np.isclose(hyper_parameters.config[param]['scale'], 1.,
-                                  atol=scale_delta):
-                        hyper_parameters.config[param]['stop'] = True
-                    hyper_parameters.config[param]['count'] += 1
-                    hyper_parameters.config[param]['scale'] = .5 * \
-                        np.exp(-hyper_parameters.config[param]
-                               ['count'] * 2) + 1
-                    # delta[param] = max(delta[param] / 2, min_delta)
-                    hyper_parameters.config[param]['buffer'].clear()
-                    # continue
-                    hyper_parameters.config[param]['current'] /= \
-                        hyper_parameters.config[param]['scale'] ** 3
+            current = hyper_parameters.config[param].current * (
+                hyper_parameters.config[param].scale ** scale_power[i])
+            hyper_parameters.config[param].current = current
+            # if np.isclose(cur_rank, 0.0):
+            #     print("RANK ZERO BACK UP")
+            #     hyper_parameters.config[param]['current'] *= \
+            #         hyper_parameters.config[param]['scale'] ** 2
+            #     continue
+            # hyper_parameters.config[param]['buffer'].append(cur_rank)
+        zeta = np.cumprod(rank_history) ** power
+        if np.less(zeta[-1], min_delta):
+            for param in hyper_parameters.config.keys():
+                if np.isclose(hyper_parameters.config[param].scale, 1.,
+                              atol=scale_delta):
+                    hyper_parameters.config[param].stop = True
+                hyper_parameters.config[param].count += 1
+                hyper_parameters.config[param].scale = .5 * \
+                    np.exp(-hyper_parameters.config[param].count * 2) + 1
 
-                hyper_parameters.config[param]['current'] *= \
-                    hyper_parameters.config[param]['scale']
-                if param == 'init_lr':
-                    training_agent.config['init_lr'] = \
-                        hyper_parameters.config[param]['current']
-                elif param == 'weight_decay':
-                    training_agent.config['optimizer_kwargs']['weight_decay'
-                                                              ] = \
-                        hyper_parameters.config[param]['current']
-        for param, conf in hyper_parameters.config.items():
-            print(param)
-            print(conf)
-        if all([config['stop'] for _, config in
+            # hyper_parameters.config[param]['current'] *= \
+            #     hyper_parameters.config[param]['scale']
+            # if param == 'init_lr':
+            #     training_agent.config['init_lr'] = \
+            #         hyper_parameters.config[param]['current']
+            # elif param == 'weight_decay':
+            #     training_agent.config['optimizer_kwargs']['weight_decay'
+            #                                               ] = \
+            #         hyper_parameters.config[param]['current']
+        # for param, conf in hyper_parameters.config.items():
+        #     print(param)
+        #     print(conf)
+        if all([config.stop for _, config in
                 hyper_parameters.config.items()]):
             break
-    with (training_agent.output_path / 'lrrt.csv').open('w+') as f:
-        f.write('lr,rank,msg\n')
-        for (lr, rank, msg) in output_history:
-            f.write(f'{lr},{rank},{msg}\n')
-    return hyper_parameters
+    # with (training_agent.output_path / 'lrrt.csv').open('w+') as f:
+    #     f.write('lr,rank,msg\n')
+    #     for (lr, rank, msg) in output_history:
+    #         f.write(f'{lr},{rank},{msg}\n')
+    return training_agent.config
